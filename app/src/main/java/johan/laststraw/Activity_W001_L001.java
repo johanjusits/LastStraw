7ebce4c17987a4fe85f553cde1d56524b989a487
Johan Samuelsson Jusits <jsamuelsson85@yahoo.se>
2014-12-28 23:11:44 +0100

    added xlarge support for w1 lv1-4

--- app/src/main/java/johan/laststraw/Activity_W001_L001.java
+++ app/src/main/java/johan/laststraw/Activity_W001_L001.java
@@ -2354,7 +2354,10 @@ public class Activity_W001_L001 extends Activity implements View.OnClickListener
                     dialog.getWindow().setLayout(1000,600);
                 } else if (density.equals("hdpi")) {
                     dialog.getWindow().setLayout(500, 300);
+                } else if (density.equals("mdpi")) {
+                    dialog.getWindow().setLayout(400, 250);
                 }
+
                 dialog.setCancelable(false);
 
                 TextView tvPlayer = (TextView) dialog.findViewById(R.id.tvPlayer);
@@ -2493,6 +2496,8 @@ public class Activity_W001_L001 extends Activity implements View.OnClickListener
                     dialog.getWindow().setLayout(1000,600);
                 } else if (density.equals("hdpi")) {
                     dialog.getWindow().setLayout(500, 300);
+                } else if (density.equals("mdpi")) {
+                    dialog.getWindow().setLayout(400, 250);
                 }
                 dialog.setCancelable(false);
 
